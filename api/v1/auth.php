133bc15da01abacb4b4a6a4d654bb0b50d4d4d18
Laura <brejcakova.laura@gmail.com>
2024-05-12 21:35:56 +0200

    password change

--- api/v1/auth.php
+++ api/v1/auth.php
@@ -16,6 +16,9 @@ switch ($method) {
       case 'register':
         handleRegistration();
         break;
+      case 'passwordChange':
+        handlePasswordChange();
+        break;
       default:
         handleInvalidEndpoint();
         break;
@@ -25,6 +28,83 @@ switch ($method) {
     handleInvalidRequestMethod();
     break;
 }
+function handlePasswordChange()
+{
+  $data = json_decode(file_get_contents('php://input'), true);
+  //timestamp-prvykrat overenie hesla,tak zacne odpocet a ak do 5 min nepride nove heslo,tak sa akcia nekona...404 zmeskane
+  $idUser = $data['idUser'];
+  $password = $data['password'];
+  $newPassword = $data['newPassword'];
+
+  $user = getUserById($idUser);
+  if ($user) {
+    if (isPasswordChangeRequestValid($user['password_change_timestamp'])) {
+      if (password_verify($password, $user['password'])) {
+        // Password verification successful, update the password
+        $hashedNewPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+        updateUserPassword($idUser, $hashedNewPassword);
+
+        $responseData = [
+          'success' => 'Password changed successfully',
+          'user' => [
+            'id' => $user['user_id'],
+            'username' => $user['name'],
+            'email' => $user['mail'],
+          ],
+        ];
+      } else {
+        // Invalid current password
+        $responseData = [
+          'error' => 'Invalid current password',
+        ];
+        http_response_code(400);
+      }
+    } else {
+      // Password change request expired
+      $responseData = [
+        'error' => 'Password change request expired',
+      ];
+      http_response_code(404);
+    }
+  } else {
+    // User not found
+    $responseData = [
+      'error' => 'User not found',
+    ];
+    http_response_code(404);
+  }
+
+  echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+}
+function getUserById($idUser)
+{
+  global $db;
+  $stmt = $db->prepare("SELECT * FROM users WHERE user_id = ?");
+  $stmt->bind_param("i", $idUser);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  return $result->fetch_assoc();
+}
+function isPasswordChangeRequestValid($timestamp)
+{
+  $allowedTimeframe = 5 * 60; // 5 minutes in seconds
+  $currentTime = time();
+  $timeDifference = $currentTime - strtotime($timestamp);
+
+  // Check if the time difference is within the allowed timeframe
+  return $timeDifference <= $allowedTimeframe;
+}
+
+function updateUserPassword($idUser, $newPassword)
+{
+  global $db;
+  $stmt = $db->prepare("UPDATE users SET password = ? WHERE user_id = ?");
+  $stmt->bind_param("si", $newPassword, $idUser);
+  $stmt->execute();
+}
+
+
 
 function handleLogin()
 {
@@ -86,7 +166,7 @@ function handleRegistration()
     ];
     http_response_code(400);
   } else {
-    $userId = insertUser($username, $password, $email); 
+    $userId = insertUser($username, $password, $email);
 
     // Generate token for the registered user and save it into DB
     $token = generateToken();
@@ -103,41 +183,41 @@ function handleRegistration()
 
 function getUserByName($username)
 {
-    global $db;
-    $stmt = $db->prepare("SELECT * FROM users WHERE name = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    return $result->fetch_assoc();
+  global $db;
+  $stmt = $db->prepare("SELECT * FROM users WHERE name = ?");
+  $stmt->bind_param("s", $username);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  return $result->fetch_assoc();
 }
 
 function getUserByEmail($email)
 {
-    global $db;
-    $stmt = $db->prepare("SELECT * FROM users WHERE mail = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    return $result->fetch_assoc();
+  global $db;
+  $stmt = $db->prepare("SELECT * FROM users WHERE mail = ?");
+  $stmt->bind_param("s", $email);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  return $result->fetch_assoc();
 }
 
 function insertUser($username, $password, $email)
 {
-    global $db;
-    $stmt = $db->prepare("INSERT INTO users (name, password, mail) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $username, password_hash($password, PASSWORD_DEFAULT), $email);
-    $stmt->execute();
-    return $stmt->insert_id;
+  global $db;
+  $stmt = $db->prepare("INSERT INTO users (name, password, mail) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $username, password_hash($password, PASSWORD_DEFAULT), $email);
+  $stmt->execute();
+  return $stmt->insert_id;
 }
 
 function saveToken($token, $userId)
 {
-    global $db;
-    date_default_timezone_set('Europe/Bratislava');
-    $expiration = date('Y-m-d H:i:s', strtotime('+1 day')); // Token expiration in 1 day
-    $stmt = $db->prepare("INSERT INTO tokens (token, user_id, expiration_timestamp) VALUES (?, ?, ?)");
-    $stmt->bind_param("sis", $token, $userId, $expiration);
-    $stmt->execute();
+  global $db;
+  date_default_timezone_set('Europe/Bratislava');
+  $expiration = date('Y-m-d H:i:s', strtotime('+1 day')); // Token expiration in 1 day
+  $stmt = $db->prepare("INSERT INTO tokens (token, user_id, expiration_timestamp) VALUES (?, ?, ?)");
+  $stmt->bind_param("sis", $token, $userId, $expiration);
+  $stmt->execute();
 }
 
 function handleInvalidEndpoint()
